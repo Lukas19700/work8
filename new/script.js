5a6701459ff4cd9fd54327ecf9465a41098de7ef
Lukas <lukasjanosiks4@gmail.com>
2022-10-22 12:17:05 +0200

    first commit

---
+++ new/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  alert( 'Yeah, you clicked me');
+}
+function myFunction1() {
+  document.getElementById("first").innerHTML = "changed";
+}
+function myFunction2() {
+  document.getElementById("first").style.backgroundColor = "red";
+}
+
